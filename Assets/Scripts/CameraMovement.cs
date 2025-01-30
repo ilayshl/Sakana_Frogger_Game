1f8334126e9687e36551969a5b65f0e243143fb8
Ilay Sh <ilayshlomo@gmail.com>
2025-01-30 03:11:51 +0200

    29.1 pc
    -remade all the assets to fit their actual size
    -remade the camera movement via independent script
    -messed everything up but will make it work better than ever

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float movementSpeed;
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 targetPosition = new Vector3(target.position.x, target.position.y, -10) + offset;
+        float distance = Vector3.Distance(currentPosition, targetPosition);
+        Debug.Log(distance);
+        transform.position = 
+        Vector3.MoveTowards(currentPosition, targetPosition, movementSpeed * Time.deltaTime * distance);
+    }
+
+    public void ChangeOffset(Vector3 change){
+        offset+=change;
+    }
+}
